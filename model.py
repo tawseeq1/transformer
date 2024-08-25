f41e1980516ce15d42c57d74240198314f347e71
Tawseeq <92048271+tawseeq1@users.noreply.github.com>
2024-08-25 12:56:42 +0530

    Add files via upload
    Incomplete

---
+++ model.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn
+import math
+
+class InputEmbeddings(nn.Module):
+    
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+    
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+    
+class PositionalEmbedding(nn.Module):
+    
+    def __init__(self, d_model: int, seq_len: int, dropout: float):
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+        
+        pe = torch.zeros(seq_len, d_model)
+        position = torch.arange(0, seq_len -1, dtype=torch.float).unsqueeze(1)
+        din_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        # i_no = torch.arange(0, d_model, 2, dtype=torch.float).unsqueeze(0)
+        # din_term = torch.pow(10000.0, (2 * i_no / d_model))
+        # position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        # pe = position / din_term
+        pe[:, 0::2] = torch.sin(position * din_term)
+        pe[:, 1::2] = torch.cos(position * din_term)
+        
+        pe = pe.unsqueeze(0) #new size = (1, seqlen,  d_model)
+        self.register_buffer('pe', pe)
+        
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
+        return self.dropout(x)
+    
+class LayerNormalization(nn.Module):
+    def __init__(self, eps: float = 10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1))
+        self.bias = nn.Parameter(torch.zeros(1))
+    def forward(self, x):
+        mean = x.mean(dim = -1, keepdim = True)
+        std = x.std(dim = -1, keepdim = True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
+
+class FeedForwardBlock(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(self.dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+    def forward(self, x):
+        # (batch, seq_len, d_model) --> (batch, seq_len, d_ff) --> (batch, seq_len, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+    
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model: int, h: int, dropout: float ) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        assert d_model % h == 0, "d_model is not divisible by h"
+        self.d_k = d_model / h
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        self.w_o = nn.Linear(d_model, d_model)
+        self.dropout = nn.Dropout(dropout)
+    @staticmethod
+    def attention(query, key, value, mask, dropout = nn.Dropout):
+        d_k = query.shape[-1]
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores.masked_fill(mask == 0, -1e9)
+        attention_scores = attention_scores.softmax(dim = -1)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        return (attention_scores @ value), attention_scores
+        
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q)
+        key = self.w_k(k)
+        value = self.w_v(v)
+        # (batch, seq_len, d_model) --> (batch, seq_len, h, d_k) --> (batch, h, seq_len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+        
+        x, self.attention_scores = MultiHeadAttention.attention(query, key, value, mask, self.dropout)
+        x = x.transpose(1, 2).contigious().view(x.shape[0], -1, self.h * self.d_model)
+        return self.w_o(x)
+        
+class RseidualConnection(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        
+        
