e93fe55ba10aa73dcec73e5304810c2273287685
Tawseeq <92048271+tawseeq1@users.noreply.github.com>
2024-09-18 22:23:11 +0530

    Update train.py

--- train.py
+++ train.py
@@ -6,11 +6,32 @@ from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
 from pathlib import Path
+from torch.utils.data import Dataset, DataLoader, random_split
+def get_all_sentences(ds, lang):
+    for item in ds:
+        yield item['translate'][lang]
 def build_tokenizer(config, ds, lang):
     tokenizer_path = Path(config['tokenizer_path'].format(lang))
     if not Path.exists(tokenizer_path):
         tokenizer = Tokenizer(WordLevel(unk_token = "[UNK]"))
         pre_tokenizer = Whitespace()
-        trainer = WordLevelTrainer(special_tokens = ["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency = 2)
+        trainer = WordLevelTrainer(min_frequency = 2, special_tokens = ["[UNK]", "[PAD]", "[SOS]", "[EOS]"])
+        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
+        tokenizer.save(tokenizer_path)
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+    return tokenizer
+def get_ds(config):
+    ds_raw = load_dataset('opus_books', f'{config["lang_src"]}-{config["lang_tgt"]}', split='train')
+    tokenizer_source = build_tokenizer(config, ds_raw, config["lang_src"])
+    tokenizer_target = build_tokenizer(config, ds_raw, config["lang_tgt"])
+
+    train_ds_size = int(0.9 * len(ds_raw))
+    val_ds_size = len(ds_raw) - train_ds_size
+    train_ds_raw, val_ds_raw = ds_raw[:train_ds_size], ds_raw[train_ds_size:]
+    train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
+
+
+
 
 
