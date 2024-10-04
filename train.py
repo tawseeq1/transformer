23b025cc09de9b01d1441e61ba76d5d42acd63b5
Tawseeq <92048271+tawseeq1@users.noreply.github.com>
2024-10-04 10:24:57 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,37 +1,59 @@
 import torch
 import torch.nn as nn
+from dataset import Translator_Dataset
+from torch.utils.data import Dataset, DataLoader, random_split
+
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
 from pathlib import Path
-from torch.utils.data import Dataset, DataLoader, random_split
+
+from model import build_transformer
+
+
 def get_all_sentences(ds, lang):
     for item in ds:
-        yield item['translate'][lang]
+        yield item['translation'][lang]
 def build_tokenizer(config, ds, lang):
     tokenizer_path = Path(config['tokenizer_path'].format(lang))
     if not Path.exists(tokenizer_path):
-        tokenizer = Tokenizer(WordLevel(unk_token = "[UNK]"))
-        pre_tokenizer = Whitespace()
-        trainer = WordLevelTrainer(min_frequency = 2, special_tokens = ["[UNK]", "[PAD]", "[SOS]", "[EOS]"])
-        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
-        tokenizer.save(tokenizer_path)
+        tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
+        tokenizer.pre_tokenizer = Whitespace()
+        trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
+        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer = trainer)
+        tokenizer.save(str(tokenizer_path))
     else:
         tokenizer = Tokenizer.from_file(str(tokenizer_path))
     return tokenizer
-def get_ds(config):
-    ds_raw = load_dataset('opus_books', f'{config["lang_src"]}-{config["lang_tgt"]}', split='train')
-    tokenizer_source = build_tokenizer(config, ds_raw, config["lang_src"])
-    tokenizer_target = build_tokenizer(config, ds_raw, config["lang_tgt"])
-
+def get_dataset(config):
+    ds_raw = load_dataset(f"{config['datasource']}", f"{config['lang_src']}-{config['lang_tgt']}", split='train')
+    tokenizer_src = build_tokenizer(config, ds_raw, config['lang_src'])
+    tokenizer_tgt = build_tokenizer(config, ds_raw, config['lang_tgt'])
     train_ds_size = int(0.9 * len(ds_raw))
     val_ds_size = len(ds_raw) - train_ds_size
-    train_ds_raw, val_ds_raw = ds_raw[:train_ds_size], ds_raw[train_ds_size:]
     train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
 
+    train_ds = Translator_Dataset(train_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+    val_ds = Translator_Dataset(val_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
 
+    max_len_src = 0
+    max_len_tgt = 0
 
+    for item in ds_raw:
+        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+        tgt_ids = tokenizer_tgt.encode(item['translation'][config['lang_src']]).ids
+        max_len_src = max(max_len_src, len(src_ids))
+        max_len_tgt = max(max_len_tgt, len(tgt_ids))
 
+    print(f'Max length of source sentence: {max_len_src}')
+    print(f'Max length of target sentence: {max_len_tgt}')
 
+    train_dataloader = DataLoader(train_ds, batch_size=config['batch_size'], shuffle=True)
+    val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=True)
+    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
+
+def get_model(config, vocab_src_len, vocab_tgt_len):
+    model = build_transformer(vocab_src_len, vocab_tgt_len, config['seq_len'], config['seq_len'], d_model=config['d_model']):
+    return model
